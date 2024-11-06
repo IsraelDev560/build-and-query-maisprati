2d9562e8750ea6f936405be8ed06d43a5dcb2d0c
devisrael <devisrael560@gmail.com>
2024-11-06 11:19:49 -0300

    add: doing 2

--- exercicio02.sql
+++ exercicio02.sql
@@ -27,4 +27,67 @@ CREATE TABLE IF NOT EXISTS `exercicio02`.`itens_comanda` (
 );
 
 
+INSERT INTO `cardapio`(
+    nome_cafe, descricao, preco_unitario 
+) VALUES (
+    "Café com açúcar", "Café preto com açúcar.", 2.50 
+);
+INSERT INTO `cardapio`(
+    nome_cafe, descricao, preco_unitario 
+) VALUES (
+    "Pão de sal", "Pão de sal", 0.50 
+);
+INSERT INTO `cardapio`(
+    nome_cafe, descricao, preco_unitario 
+) VALUES (
+    "Bauru de Frango", "Bauru de frango com catupiry", 3.20 
+);
 
+INSERT INTO `comanda`(
+    data_comanda, mesa, nome_cliente
+) VALUES (
+    "2024-11-06", 1, "Israel"
+);
+INSERT INTO `comanda`(
+    data_comanda, mesa, nome_cliente
+) VALUES (
+    "2024-12-06", 3, "Dênis"
+);
+INSERT INTO `comanda`(
+    data_comanda, mesa, nome_cliente
+) VALUES (
+    "2024-11-20", 2, "Ajaques"
+);
+
+INSERT INTO `itens_comanda`(
+    comanda_id, cardapio_id, quantidade, preco_total
+) VALUES (
+    1, 1, 1, 2.50
+);
+
+INSERT INTO `itens_comanda`(
+    comanda_id, cardapio_id, quantidade, preco_total
+) VALUES (
+    2, 3, 2, 6.40
+);
+
+INSERT INTO `itens_comanda`(
+    comanda_id, cardapio_id, quantidade, preco_total
+) VALUES (
+    3, 2, 3, 1.50
+);
+
+-- 1: Faça uma listagem do cardápio ordenada por nome;
+SELECT nome_cafe FROM `cardapio`;
+
+-- 2: Apresente todas as comandas (código, data, mesa e nome do cliente)
+-- e os itens da comanda (código comanda, nome do café, descricão, quantidade,
+-- preço unitário e preço total do café) destas ordenados data e código da comanda e,
+-- também o nome do café;
+
+SELECT c.data_comanda, i.comanda_id, ca.nome_cafe 
+FROM `comanda` c 
+INNER JOIN `itens_comanda` i ON i.comanda_id = c.comanda_id 
+INNER JOIN `cardapio` ca ON i.cardapio_id = ca.cardapio_id;
+
+SELECT * FROM comanda, itens_comanda;
